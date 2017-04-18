159dd41feae81ec02c8affab0c106785715083d7
Ove Nesse <ons@datarespons.no>
2017-04-18 17:14:17 +0200

    added BLE support

---
+++ frff.cpp
@@ -0,0 +1,18 @@
+// #include "mbed.h"
+
+// #include "ble/ble/BLE.h"
+
+// DigitalOut led(LED1);
+// BLE device();
+
+// int main()
+// {
+//     NVIC_SetPriority(DebugMonitor_IRQn, 7ul);
+//     device.
+//     while(true)
+//     {
+        
+//         led = !led;
+//         wait(2.0);
+//     }
+// }
--- mbed_config.h
+++ mbed_config.h
@@ -12,5 +12,6 @@
 #define MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE 9600            // set by library:platform
 #define MBED_CONF_PLATFORM_STDIO_FLUSH_AT_EXIT      1               // set by library:platform
 #define MBED_CONF_PLATFORM_STDIO_CONVERT_NEWLINES   0               // set by library:platform
+#define MBED_CONF_EVENTS_PRESENT                    1
 
 #endif
